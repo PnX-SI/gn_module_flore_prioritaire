95ef1a23dabca8e40df6132c7c41deedd15f50f6
Cendrine HOARAU <c.hoarau@cbn-alpin.fr>
2022-06-30 11:48:10 +0200

    Feat: final version of migration scripts v1 to v2

---
+++ backend/gn_module_priority_flora/migrations/data/migrate_v1_to_v2/02_migrate_users.sql
@@ -0,0 +1,145 @@
+-- Migrate data from migrate_v1_florepatri and migrate_v1_utilisateurs
+-- into GeoNature v2 Priority Flora module schema.
+-- Usage:
+--  1. go to sql directory: cd backend/gn_module_priority_flora/migrations/data/migrate_v1_to_v2
+--  2. migrate data with scripts #3 :
+--    export PGPASSWORD="<db_pass>"; psql -h "<db_host>" -U "<db_user>" -d "<db_name>" -f "03_add_users.sql"
+--
+-- where:
+-- - <db_pass>: GeoNature v2 database user password.
+-- - <db_host>: GeoNature v2 database host name. Ex.: "localhost".
+-- - <db_user>: GeoNature v2 database user name with write access. Ex.: "geonatadmin".
+-- - <db_name>: GeoNature v2 database name. Ex.: "geonature2db".
+
+BEGIN;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Insert organisms'
+INSERT INTO utilisateurs.bib_organismes (
+        nom_organisme,
+        adresse_organisme,
+        cp_organisme,
+        ville_organisme,
+        tel_organisme,
+        fax_organisme,
+        email_organisme
+    )
+    SELECT DISTINCT
+        o.nom_organisme,
+        nullif(o.adresse_organisme, ''),
+        nullif(o.cp_organisme, ''),
+        nullif(upper(o.ville_organisme), ''),
+        regexp_replace(
+            replace(
+                nullif(
+                    trim(both from o.tel_organisme),
+                    ''
+                ),
+                '.',
+                ' '
+            ),
+            '^([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})$',
+            '\1 \2 \3 \4 \5'
+        ),
+        regexp_replace(
+            replace(
+                nullif(
+                    trim(both from o.fax_organisme),
+                    ''
+                ),
+                '.',
+                ' '
+            ),
+            '^([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})$',
+            '\1 \2 \3 \4 \5'
+        ),
+        nullif(o.email_organisme, '')
+    FROM migrate_v1_utilisateurs.bib_organismes AS o
+    WHERE NOT EXISTS (
+            SELECT 'X'
+            FROM utilisateurs.bib_organismes AS bo
+            WHERE upper(bo.nom_organisme) = upper(o.nom_organisme)
+        )
+;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Refresh "bib_organismes" table sequence (=auto-increment)'
+SELECT setval(
+    'utilisateurs.bib_organismes_id_organisme_seq',
+    (SELECT max(id_organisme) + 1 FROM utilisateurs.bib_organismes),
+    true
+) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Insert roles'
+INSERT INTO utilisateurs.t_roles (
+        groupe,
+        identifiant,
+        nom_role,
+        prenom_role,
+        desc_role,
+        pass,
+        email,
+        id_organisme,
+        remarques,
+        active,
+        champs_addi
+    )
+    SELECT DISTINCT
+        mr.groupe,
+        nullif(mr.identifiant, ''),
+        nullif(upper(mr.nom_role), ''),
+        nullif(mr.prenom_role, ''),
+        nullif(mr.desc_role, ''),
+        nullif(mr.pass, ''),
+        nullif(mr.email, ''),
+        (SELECT bo.id_organisme from utilisateurs.bib_organismes as bo where upper(bo.nom_organisme) = upper(mbo.nom_organisme)),
+        'Imported from GeoNature v1.',
+        True,
+        json_build_object(
+            'migrateOriginalInfos', json_build_object(
+                'dateInsert', mr.date_insert,
+                'dateUpdate', mr.date_update,
+                'lastAccess', mr.dernieracces,
+                'roleId', mr.id_role,
+                'organismId', mr.id_organisme,
+                'organismName', mbo.nom_organisme
+            )
+        )::jsonb
+    FROM migrate_v1_utilisateurs.t_roles AS mr
+        LEFT JOIN migrate_v1_utilisateurs.bib_organismes as mbo
+            ON mbo.id_organisme = mr.id_organisme
+    WHERE NOT EXISTS (
+        SELECT 'X'
+        FROM utilisateurs.t_roles AS u
+        WHERE (
+                upper(u.nom_role) = upper(mr.nom_role)
+                AND upper(u.prenom_role) = upper(mr.prenom_role)
+            )
+    )
+;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Create users UUID if not set'
+UPDATE utilisateurs.t_roles
+SET uuid_role = uuid_generate_v4()
+WHERE uuid_role IS NULL 
+    AND remarques = 'Imported from GeoNature v1.';
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'Refresh "t_roles" table sequence (=auto-increment)'
+SELECT setval(
+    'utilisateurs.t_roles_id_role_seq',
+    (SELECT max(id_role) + 1 FROM utilisateurs.t_roles),
+    true
+) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'COMMIT if all goes well !'
+COMMIT;
---
+++ backend/gn_module_priority_flora/migrations/data/migrate_v1_to_v2/03_migrate_data.sql
@@ -0,0 +1,339 @@
+-- Migrate data from migrate_v1_florepatri and migrate_v1_utilisateurs
+-- into GeoNature v2 Priority Flora module schema.
+-- Usage:
+--  1. go to sql directory: cd backend/gn_module_priority_flora/migrations/data/migrate_v1_to_v2
+--  2. migrate data with scripts #2 :
+--    export PGPASSWORD="<db_pass>"; psql -h "<db_host>" -U "<db_user>" -d "<db_name>" -f "02_migrate_data.sql"
+--
+-- where:
+-- - <db_pass>: GeoNature v2 database user password.
+-- - <db_host>: GeoNature v2 database host name. Ex.: "localhost".
+-- - <db_user>: GeoNature v2 database user name with write access. Ex.: "geonatadmin".
+-- - <db_name>: GeoNature v2 database name. Ex.: "geonature2db".
+
+-- TODO :
+-- 1. vérifier les noms des champs. !!L.87 + L.159 "cor_ap_perturb"!!
+-- 2. ajouter des not exists pour éviter les erreurs d'insertion.
+-- 3. analyser le contenu migré pour voir si on ne peut pas l'améliorer.
+-- 4. lancer le script #2 02_migrate.sql et le corriger
+-- 5. vérifier le bon fonctionne du module (front et back) avec les données => le corriger si erreurs...
+-- 6. migrer dans branche refactor les modules SFT, SHT et enfin SHS.
+
+BEGIN;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'PRIORITY_FLORA => Insert data into "t_zprospect"'
+INSERT INTO pr_priority_flora.t_zprospect (
+    indexzp,
+    date_min,
+    date_max,
+    topo_valid,
+    initial_insert,
+    cd_nom,
+    geom_local,
+    geom_4326,
+    geom_point_4326
+)
+    SELECT
+        indexzp,
+        dateobs,
+        dateobs,
+        topo_valid,
+        saisie_initiale,
+        cd_nom,
+        the_geom_2154,
+        st_transform(geom_mixte_3857, 4326),
+        st_transform(geom_point_3857, 4326)
+    FROM migrate_v1_florepatri.t_zprospection AS mtz
+    WHERE supprime = 'false'
+        AND NOT EXISTS (
+            SELECT 'X'
+            FROM pr_priority_flora.t_zprospect AS tz
+            WHERE tz.indexzp = mtz.indexzp
+        )
+    ;
+
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'PRIORITY_FLORA => Update sequence for "t_zprospect"'
+SELECT setval(
+    'pr_priority_flora.t_zprospect_indexzp_seq',
+    (SELECT max(indexzp) FROM pr_priority_flora.t_zprospect),
+    true
+) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'PRIORITY_FLORA => Insert data into "cor_zp_obs"'
+WITH coresp AS (
+	SELECT
+        tr.id_role,
+        CAST(tr.champs_addi-> 'migrateOriginalInfos' ->> 'roleId' AS INT) AS roleId
+    from utilisateurs.t_roles AS tr
+)
+
+INSERT INTO pr_priority_flora.cor_zp_obs (
+    indexzp,
+    id_role
+)
+    SELECT
+        mcor.indexzp,
+		coresp.id_role
+    FROM migrate_v1_florepatri.cor_zp_obs AS mcor
+        JOIN migrate_v1_florepatri.t_zprospection AS tzp
+            ON tzp.indexzp = mcor.indexzp
+        JOIN coresp
+        	ON coresp.roleId = mcor.codeobs
+    WHERE tzp.supprime = 'false'
+        AND NOT EXISTS (
+            SELECT 'X'
+            FROM pr_priority_flora.cor_zp_obs AS pf
+            WHERE pf.indexzp = mcor.indexzp
+                AND pf.id_role = mcor.codeobs
+        )
+    ;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'PRIORITY_FLORA => Insert data into "t_apresence"'
+INSERT INTO pr_priority_flora.t_apresence(
+    indexap,
+    area,
+    topo_valid,
+    altitude_min,
+    altitude_max,
+    frequency,
+    "comment",
+    indexzp,
+    id_nomenclatures_pente,
+    id_nomenclatures_counting,
+    id_nomenclatures_habitat,
+    id_nomenclatures_phenology,
+    id_history_action,
+    total_min,
+    total_max,
+    geom_local,
+    geom_4326,
+    geom_point_4326
+)
+    SELECT
+        indexap,
+        surfaceap::FLOAT,
+        topo_valid,
+        altitude_retenue,
+        altitude_retenue,
+        frequenceap,
+        remarques,
+        indexzp,
+        NULL, -- Pas de notion de pente
+        ref_nomenclatures.get_id_nomenclature('FLORE_PATRI_METHODO_DENOM',mta.id_comptage_methodo::text), -- counting
+        NULL,
+        ref_nomenclatures.get_id_nomenclature('TYPE_PHENOLOGIE',mta.codepheno::text), -- counting
+        NULL, -- id_history_action
+        total_steriles+total_fertiles , --total_min
+        total_steriles+total_fertiles, -- total max
+        the_geom_2154,
+        st_transform(the_geom_3857, 4326),
+        st_centroid(st_transform(the_geom_3857, 4326))
+    FROM migrate_v1_florepatri.t_apresence AS mta
+    WHERE supprime = 'false'
+        AND NOT EXISTS (
+            SELECT 'X'
+            FROM pr_priority_flora.t_apresence AS ta
+            WHERE ta.indexap = mta.indexap
+                AND ta.indexzp = mta.indexzp
+        )
+    ;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'PRIORITY_FLORA => Update sequence for "t_apresence"'
+SELECT setval(
+    'pr_priority_flora.t_apresence_indexap_seq',
+    (SELECT max(indexap) FROM pr_priority_flora.t_apresence),
+    true
+) ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'PRIORITY_FLORA => Insert data into "cor_ap_perturb"'
+WITH nomenclature AS (
+	select 
+		id_nomenclature,
+		b.codeper
+	from ref_nomenclatures.t_nomenclatures AS tn
+	    JOIN migrate_v1_florepatri.bib_perturbations AS b
+	        ON tn.label_default = b.description
+	    JOIN ref_nomenclatures.bib_nomenclatures_types AS bib
+	    	ON (bib.id_type = tn.id_type AND bib.mnemonique = 'TYPE_PERTURBATION')
+)
+
+INSERT INTO pr_priority_flora.cor_ap_perturb (
+    indexap,
+    id_nomenclature,
+    pres_effective
+)
+    SELECT
+        a.indexap,
+        n.id_nomenclature,
+        NULL AS pres_effective
+
+    FROM migrate_v1_florepatri.cor_ap_perturb AS mcor
+        JOIN pr_priority_flora.t_apresence AS a
+            ON a.indexap = mcor.indexap
+        JOIN nomenclature AS n
+        	ON n.codeper = mcor.codeper
+	WHERE NOT EXISTS (
+        SELECT 'X'
+        FROM pr_priority_flora.cor_ap_perturb AS pf
+        WHERE pf.indexap = mcor.indexap
+            AND pf.id_nomenclature = n.id_nomenclature
+    ) ;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'TAXONOMIE => Update sequence for "bib_attributs"'
+SELECT setval(
+    'taxonomie.bib_attributs_id_attribut_seq',
+    (SELECT max(id_attribut) FROM taxonomie.bib_attributs),
+    true
+) ;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'TAXONOMIE => Insert data into "bib_attributs"'
+WITH attr_values AS (
+	SELECT '{"values":[' || string_agg ('"' || ph.nom_physionomie || '"',',') || ']}' AS attr_value
+    FROM migrate_v1_florepatri.bib_physionomies AS ph
+)
+INSERT INTO taxonomie.bib_attributs (
+    nom_attribut,
+    label_attribut,
+    obligatoire,
+    desc_attribut,
+    type_attribut,
+    type_widget,
+    id_theme,
+    liste_valeur_attribut
+)
+    SELECT
+        'physionomie',
+        'Physionomie',
+        'false',
+        'Physionomie principale du taxon.',
+        'text',
+        'multiselect',
+        1,
+        av.attr_value
+    FROM attr_values AS av
+    WHERE NOT EXISTS (
+            SELECT 'X'
+            FROM taxonomie.bib_attributs AS ba
+            WHERE ba.nom_attribut = 'physionomie'
+        )
+    ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'TAXONOMIE => Insert data into "cor_taxon_attribut"'
+INSERT INTO taxonomie.cor_taxon_attribut (
+    cd_ref,
+    valeur_attribut,
+    id_attribut
+)
+    SELECT DISTINCT
+        tx.cd_ref,
+        string_agg(bp.nom_physionomie, ' & ') AS valeur_attribut,
+        (SELECT id_attribut FROM taxonomie.bib_attributs WHERE nom_attribut = 'physionomie')
+    FROM migrate_v1_florepatri.cor_ap_physionomie AS cap
+        JOIN migrate_v1_florepatri.t_apresence AS ta
+            ON ta.indexap = cap.indexap
+        JOIN migrate_v1_florepatri.t_zprospection AS tz
+            ON tz.indexzp = ta.indexzp
+        JOIN taxonomie.taxref AS tx
+            ON tx.cd_nom = tz.cd_nom
+        JOIN migrate_v1_florepatri.bib_physionomies AS bp
+            ON bp.id_physionomie = cap.id_physionomie
+    WHERE NOT EXISTS (
+            SELECT 'X'
+            FROM taxonomie.cor_taxon_attribut AS cor
+            WHERE cor.cd_ref = tx.cd_ref
+        )
+    GROUP BY tx.cd_ref ;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'TAXONOMIE => Update sequence for "bib_listes"'
+SELECT setval(
+    'taxonomie.bib_listes_id_liste_seq',
+    (SELECT max(id_liste) FROM taxonomie.bib_listes),
+    true
+) ;
+
+\echo '----------------------------------------------------------------------------'
+\echo 'TAXONOMIE => Insert data into "bib_listes"'
+INSERT INTO taxonomie.bib_listes (
+    nom_liste,
+    desc_liste,
+    regne,
+    code_liste
+) VALUES (
+    'Priority Flora',
+    'Liste de taxons pour le module Priority Flora.',
+    'Plantae',
+    'PRIORITY_FLORA'
+) 
+ON CONFLICT (nom_liste) DO NOTHING ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'TAXONOMIE => Insert taxon names into "bib_noms"'
+    INSERT INTO taxonomie.bib_noms (
+    cd_nom,
+    cd_ref,
+    nom_francais,
+    "comments"
+)
+
+	SELECT
+        t.cd_nom,
+        t.cd_ref,
+        btf.francais,
+        'Imported by Priority Flora.'
+    FROM taxonomie.taxref  AS t
+        JOIN migrate_v1_florepatri.bib_taxons_fp AS btf
+            ON btf.cd_nom = t.cd_nom
+    WHERE NOT EXISTS (
+            SELECT 'X'
+            FROM taxonomie.bib_noms AS bn
+            WHERE bn.cd_nom = btf.cd_nom
+        )
+    ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'TAXONOMIE => Insert data into "cor_nom_liste"'
+WITH code_liste AS (
+    SELECT id_liste 
+    FROM taxonomie.bib_listes 
+    WHERE code_liste = 'PRIORITY_FLORA'
+)
+INSERT INTO taxonomie.cor_nom_liste (
+    id_liste,
+    id_nom
+)
+    SELECT
+        cl.id_liste,
+        b.id_nom 
+    FROM taxonomie.bib_noms AS b
+        JOIN migrate_v1_florepatri.bib_taxons_fp AS t
+            ON b.cd_nom = t.cd_nom,
+        code_liste AS cl
+    WHERE NOT EXISTS (
+            SELECT 'X'
+            FROM taxonomie.cor_nom_liste AS cnl, code_liste AS scl
+            WHERE cnl.id_liste = scl.id_liste
+                AND cnl.id_nom = b.id_nom
+        )
+    ;
+
+
+\echo '----------------------------------------------------------------------------'
+\echo 'COMMIT if all goes well !'
+COMMIT;
